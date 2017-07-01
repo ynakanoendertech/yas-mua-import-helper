1012526766f4344c8cc9f9a7eb9eeda767aa3a8d
Yasuhiro Nakano <ynakano@endertech.com>
2017-06-30 18:52:33 -0700

    Add recursive function

--- js/scripts.js
+++ js/scripts.js
@@ -7,51 +7,6 @@
 
     $(document).ready(function() {
 
-        /*
-        var a1 = [
-            "apple", "banana", "citrus"
-        ];
-        a1.shift();
-
-        var cnt = 0;
-        var final = {};
-        function makeStructure(key, path, data) {
-            // final[ key ] = 1;
-            console.log('-----');
-            console.log(key);
-            console.log(data);
-
-            console.log("final" + path + " = {};");
-            console.log(final);
-            if (! eval("final" + path) ) {
-                eval("final" + path + " = {};");
-            } else {
-
-            }
-
-            if (data.length == 0) {
-                return 0;
-            }
-            var newKey = data.shift();
-
-            var newPath = path + '["' + newKey + '"]';
-            // console.log(newKey);
-            // console.log(newPath);
-            // console.log(data);
-
-            if (cnt > 10) {
-                return 0;
-            }
-            cnt++;
-
-            makeStructure(newKey, newPath, data);
-        }
-        makeStructure("apple", '["apple"]', a1);
-        console.dir(final);
-
-        return 0;
-        */
-
         // Check for the various File API support.
         if (! (window.File && window.FileReader && window.FileList && window.Blob)) {
             console.warn('File API not supported on this browser.');
@@ -59,7 +14,28 @@
         }
 
         //
-        var categories = {};
+        window.categories = {};
+        function makeStructure(data, key, path) {
+
+            if (key === undefined) {
+                var initKey = data.shift();
+                var initPath = categories;
+                makeStructure(data, initKey, initPath);
+            } else {
+
+                if (path[ key ] === undefined) {
+                    path[ key ] = {};
+                }
+
+                if (data.length == 0) {
+                    return 0;
+                }
+
+                var newKey = data.shift();
+                var newPath = path[ key ];
+                makeStructure(data, newKey, newPath);
+            }
+        }
 
         function handleFileSelect(evt) {
 
@@ -74,9 +50,6 @@
                 reader.onload = (function(theFile) {
                     return function(e) {
 
-                        var output = "";
-                        var categories = {};
-
                         // lines
                         var lines = e.target.result;
                         lines = lines.split("\n");
@@ -84,52 +57,30 @@
 
                             // entries
                             var line = lines[i];
+                            line = line.substring(0, line.length - 1);  // remove last invisible character from line
+
                             var entries = line.split("|");
                             for (var j = 0; j < entries.length; j++) {
 
                                 // category
                                 var entry = entries[j];
+                                var category = entry.split("/");
 
-                                console.log( entry );
-                                if (categories[ entry ] === undefined) {
-                                    categories[ entry ] = 1;
-                                } else {
-                                    categories[ entry ] = categories[ entry ] + 1;
+                                if (category == "") {
+                                    break;
                                 }
 
-                                // var category = entry.split("/");
-                                // var test = {};
-                                // for (var k = 0; k < category.length; k++) {
-                                //     console.log(k, category[k] );
-                                // }
-
-                                // var test = category.map(c => `["${c}"]`).join('');
-                                // console.log(test);
-
-
-                                // var expresssionNewCategory = "categories" + test;
-
-                                // console.log(expresssionNewCategory);
-
-                                // console.log(scope);
-
-                                // var test = category.map(c => `["${c}"]`).join('');
-                                // var expresssionNewCategory = "categories" + test + "=1;";
-                                // console.log(expresssionNewCategory);
+                                makeStructure(category);
                             }
-
-
-                            output += '<li>' + lines[i] + '</li>';
                         }
 
-                        $('#list').html(output);
-
                         console.dir( categories );
+                        $('#list').html( JSON.stringify( categories, null, 4 ) );
                     };
                 })(f);
 
                 // Read as text
-                reader.readAsText(f);
+                reader.readAsBinaryString(f);
             }
         }
 
